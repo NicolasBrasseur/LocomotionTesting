ecaa0e9d48e1776104505b8b102731d1e525fe10
Nicolas Brasseur <nicolas.brasseur3@gmail.com>
2025-11-27 23:14:40 +0100

    chore: Remove dependency
    - Removed unused XR dependency

--- Assets/DownloadedAssets/ThirdPersonCharacter/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/DownloadedAssets/ThirdPersonCharacter/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -2,7 +2,6 @@
 using UnityEngine;
 #if ENABLE_INPUT_SYSTEM 
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.XR;
 #endif
 
 /* Note: animations are called via the controller for both the character and capsule using animator null checks
